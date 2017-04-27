9461ab0ccb5a14a87c25c96cdfd54be89ca19145
walidpitt <walidpitt@gmail.com>
2017-04-27 14:25:03 -0400

    Delete keys.js

--- keys.js
+++
@@ -1,8 +0,0 @@
-console.log('this is loaded');
-
-exports.twitterKeys = {
-  consumer_key: '<9BIEqSYeV0joT6xAcKFB0ncHs>',
-  consumer_secret: '<w3KBbQ1EB0Heya7T6Pj0ze9IlIFcgLEBwumiB6CrgKJsViLjJZ>',
-  access_token_key: '<2685518135-92CJiq8pUQuIQIGUspthLx6qEreoaJedLjDOXA8>',
-  access_token_secret: '<fXvE1s20rqqyRT6hyoXuqkounnHfvsHJtsP47rdo6lPiB>',
-}
